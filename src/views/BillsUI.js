702df84536d3335097cc3d1bb182010b5396490b
yann-lcrx <yannick84v@gmail.com>
2022-04-13 10:59:28 +0200

    feat: add bill sort by date

--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -20,7 +20,7 @@ const row = (bill) => {
   }
 
 const rows = (data) => {
-  return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
+  return (data && data.length) ? data.sort((a, b) => b.date.localeCompare(a.date)).map(bill => row(bill)).join("") : ""
 }
 
 export default ({ data: bills, loading, error }) => {
